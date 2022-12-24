1a1c33ce05b82cd3c77f97357e61ed1bdfdd481f
Ujjwal Adhikari <ujjwalnd@Ujjwals-MacBook-Pro.local>
2022-12-23 18:21:41 -0600

    added contact form

--- Stocks.py
+++ Stocks.py
@@ -5,8 +5,14 @@ import requests
 from streamlit_lottie import st_lottie
 from PIL import Image
 
+# use Local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style> {f.read()}</style>", unsafe_allow_html=True)
+
 st.set_page_config(page_title="Stock Search",
                    page_icon=":trident:", layout='wide')
+local_css("styles.css")
 
 st.markdown('<link rel="stylesheet" type="text/css" href="styles.css">',
             unsafe_allow_html=True)
@@ -34,13 +40,7 @@ with st.container():
         tickerData = yf.Ticker(tickerSymbol or 'aapl')
         info = tickerData.info
         tickerDf = tickerData.history(period='1d', start='2022-1-1', end='2022-12-13')
-        st.header(Stock)
-# hide and show button
-        More_Info = st.button("More Info")
-        text_placeholder = st.empty()
-        button_placeholder = st.empty()
-        if More_Info:
-            text_placeholder.markdown(info['longBusinessSummary'])
+        st.header(Stock or "AAPL")
     
 with right_column:
     st_lottie(
@@ -52,9 +52,6 @@ with right_column:
 
 # --- Actual Project ---
 with st.container():
-
-   
-
     left_column, right_column = st.columns(2)
 with left_column:
     st.header("Daily Chart")
@@ -67,9 +64,6 @@ with right_column:
 
 
 with st.container():
-    st.write("---")
-    st.header("My Other Projects")
-    st.write("##")
     image_column, text_column = st.columns((1,2))
     with image_column:
         st.image(img_Self_Picture)
@@ -82,3 +76,23 @@ with st.container():
             If you would like to talk about tech, a connection is more than welcome!
             """
         )
+
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Me")
+    st.write("##")
+
+    contact_form = """
+    <form action="https://formsubmit.co/ujjwaladhikari.code@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your Name" required>
+     <input type="email" name="email" placeholder= "Your Email" required>
+     <textarea type="text" name="message" placeholder="Your message here!"  required> </textarea>
+     <button type="submit">Send</button>
+    </form>
+    """
+    left_column,right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
