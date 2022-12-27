f740dbac109c5b00c949aec20af5d3c46e863ecf
Ujjwal Adhikari <ujjwalnd@Ujjwals-MacBook-Pro.local>
2022-12-27 17:18:06 -0600

    form submit

--- Stocks.py
+++ Stocks.py
@@ -92,7 +92,7 @@ with st.container():
     <input type="hidden" name="_captcha" value="false">
      <input type="text" name="name" placeholder="Your Name" required>
      <input type="email" name="email" placeholder= "Your Email" required>
-     <textarea type="text" name="message" placeholder="Your message here!"  required> </textarea>
+     <textarea type="text" name="Message" placeholder="Your message here!"  required> </textarea>
      <button type="submit">Send</button>
     </form>
     """
